167b1a7e5a85e61eb10011e53d922b6417f1f611
Dothingan <121930750+Dothingan@users.noreply.github.com>
2024-04-01 05:49:11 +0000

    try...catch

--- Main.java
+++ Main.java
@@ -29,5 +29,51 @@ class Main{
         int breadth = input.nextInt();
         rec1.getArea1(length, breadth);
         
+        //khi qua so phan tu trong mang
+
+        int[] arr = new int[3];
+        // Scanner input = new Scanner(System.in);
+        // for(int i = 0; i <= arr.length; i++){
+        //     arr[i] = input.nextInt();
+        // }
+        //input.close();
+
+    //try...catch: 
+    //chi sd try...catch khi ngoai vung kiem soat duoc
+    //finally: giai phong tai nguyen-dong ket noi
+
+        try{
+            for(int i = 0; i<= arr.length; i++){
+                System.out.printf("nhap phan tu arr[%d] = ", i);
+                arr[i] = input.nextInt();
+                }
+            } catch(Exception e){
+                //goi vao khi chuong trinh gap exception
+                e.printStackTrace();
+
+            // }catch(ArrayIndexOutOfBoundsException e){
+            //     //bo qua loi tran phan tu mang
+            //     e.printStackTrace();
+            // }catch(ArithmeticException e){
+            //     //bo qua loi chia cho 0
+            //     e.printStackTrace();
+            // }
+
+            } finally{
+                //duoc goi khi chuong trinh k co error hoac error
+                System.out.println("finish 1");
+            }
+            try {
+                int z = 5/0;
+                for(int i = 0; i<= arr.length; i++){
+                System.out.println(arr[i]);
+            }   
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println(e.getMessage());;
+            } finally{
+                System.out.println("finish 1");
+            }
+
     }
 }
